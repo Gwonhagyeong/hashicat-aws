16846e33545936f12be5c5fe034c3a0c4effbd02
Gwonhagyeong <haygeong.kwon@balaan.co.kr>
2021-11-23 09:47:18 +0000

    FIX

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/HAGYEONG-traning/s3-bucket/aws"
-  version = "2.11.1"
+  version = "2.7.0"
   # insert required variables here
   bucket_prefix="jsp01"
 }
